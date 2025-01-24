5b21684f95b58c30df7a7c2e450f522311b2a621
김보성 <62016657+bocastle@users.noreply.github.com>
2025-01-24 10:03:35 +0900

    [level 0] Title: 문자열 섞기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
@@ -3,16 +3,11 @@ function solution(str1, str2) {
     let text = [];
      str1.split('').map((item,index) =>{
     
-        text.push(item)
+        text.push(item+str2[index])
         
-        str2.split("").map((_item,_index)=>{
-            if(index === _index){
-         text.push(_item)       
-            }
-         
-        })
+        
     })  
-    // console.log(String(text).replaceAll(',',''))
+    
     answer = String(text).replaceAll(',','');
     return answer;
 }
