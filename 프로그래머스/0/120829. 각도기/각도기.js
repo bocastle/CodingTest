69c74f31d97c49762aa91f6d0eef14ac4a8ac0bf
김보성 <62016657+bocastle@users.noreply.github.com>
2025-02-13 00:43:58 +0900

    [level 0] Title: 각도기, Time: 2.21 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -0,0 +1,18 @@
+function solution(angle) {
+    var answer = 0;
+    console.log(angle)
+    if(0 < angle && angle < 90){
+        return 1
+    }
+    if(angle === 90){
+        return 2
+    }
+    if(90 < angle && angle < 180){
+        return 3
+    }
+    if(angle === 180){
+        return 4
+    }
+    
+    return answer;
+}
