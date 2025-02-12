c48549cada6e42c6a8f5676883526906349cb67c
김보성 <62016657+bocastle@users.noreply.github.com>
2025-02-13 00:34:18 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,16 +1,17 @@
 function solution(num_list) {
     var answer = [];
-    let num = 0;
-    let _num = 0;
+    let even = 0;
+    let odd = 0;
     for(let i = 0; i < num_list.length; i++){
         
         if(num_list[i] % 2 === 0){
-            num = num + 1
+            even = even + 1
         }else{
-            _num = _num +1
+            odd = odd + 1
         }
         
     }
-    console.log(num,_num)
-    return [num,_num];
+    answer.push(even,odd)
+    
+    return answer;
 }
