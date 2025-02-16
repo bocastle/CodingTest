8aad9575f5d7ea3aab9c62567e1f58f63aab8a76
김보성 <62016657+bocastle@users.noreply.github.com>
2025-02-17 00:20:06 +0900

    [level 0] Title: 가까운 1 찾기, Time: 1.84 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -1,14 +1,8 @@
 function solution(arr, idx) {
     let answer = -1;
-    // console.log('arr',arr)
+    
     for(let i = 0; i < arr.length; i++){
-        // console.log("arr",arr[i])
-        // console.log("idx-1",idx-1)
-        // console.log("iii",i)
-        // console.log("상태",idx - 1 < i)
-        // console.log("arr[i]",arr[i])
         if(arr[i] === 1 && idx - 1 < i){
-            console.log('넣자',i)
             return answer = i
         }
        
