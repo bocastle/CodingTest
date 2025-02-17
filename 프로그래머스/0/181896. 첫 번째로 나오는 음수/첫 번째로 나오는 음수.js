aae66d1b3db97210661960d8acef41b23b775ef7
김보성 <62016657+bocastle@users.noreply.github.com>
2025-02-18 00:01:28 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -0,0 +1,10 @@
+function solution(num_list) {
+    var answer = -1;
+    for(let i = 0; i < num_list.length; i++){
+        
+        if(Math.sign(num_list[i]) === -1){
+            return i
+        }
+    }
+    return answer;
+}
