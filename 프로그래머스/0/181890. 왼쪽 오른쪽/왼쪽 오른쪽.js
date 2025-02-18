b7d418c8d97ac0b72b36ce254d389bc3ee110850
김보성 <62016657+bocastle@users.noreply.github.com>
2025-02-19 01:00:48 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -0,0 +1,14 @@
+function solution(str_list) {
+    let answer = [];
+    for(let i = 0; i < str_list.length; i++){
+        if(str_list[i] ==='l'){    
+            return str_list.slice(0,i)
+            
+        }
+        if(str_list[i] ==='r'){
+            return str_list.slice(i+1,str_list.length)
+        }
+    }
+    // console.log("answer")
+    return answer;
+}
