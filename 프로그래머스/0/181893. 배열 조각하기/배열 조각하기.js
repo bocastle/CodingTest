e7c5cc047b2da06c9dfa13ae82710bcb2ce555c5
김보성 <62016657+bocastle@users.noreply.github.com>
2025-02-18 15:22:04 +0900

    [level 0] Title: 배열 조각하기, Time: 2.25 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
@@ -4,16 +4,11 @@ function solution(arr, query) {
     
     for(let i=0;i < query.length;i++){
         if(i % 2 === 0){
-            console.log('짝',array)
-            // array.push([0,2])
             array = array.slice(0,query[i]+1)
-        }else{
-            console.log('홀',array)
+        }else{            
             array = array.slice(query[i])
         }
     }
-    
-    console.log("array:::",array)
-    
+
     return array;
 }
