6beab61c9ee083811c73e8abd6d97888d2864c4c
김보성 <62016657+bocastle@users.noreply.github.com>
2025-02-21 15:38:40 +0900

    [level 0] Title: 배열 회전시키기, Time: 3.33 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -2,18 +2,13 @@ function solution(numbers, direction) {
     var answer = [];
     console.log(direction)
     if(direction === 'right'){
-        
         let num = numbers.pop()
         numbers.unshift(num)
-        // console.log("numbers",numbers)
     }
     if(direction === 'left'){
-        
-        // console.log("numbers",numbers.shift())
         let num = numbers.shift()
         numbers.push(num)
-        // console.log("numbers",numbers.push(numbers.shift()))
-        // console.log("numbers",numbers)
+
     }
     return numbers;
 }
