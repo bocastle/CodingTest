69374e0678f2cfef429b59bcaef55c420d506606
김보성 <62016657+bocastle@users.noreply.github.com>
2025-03-05 00:53:50 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -0,0 +1,10 @@
+function solution(my_string) {
+    let answer = 0;
+    for(let i = 0; i < my_string.length; i++){
+        
+        if(my_string[i] < 10){
+            answer += Number(my_string[i])
+        }
+    }
+    return answer;
+}
