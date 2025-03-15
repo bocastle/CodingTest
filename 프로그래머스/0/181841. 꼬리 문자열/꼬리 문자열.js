2ca578e44318134a8eb5cb42f8f29205ac108c5f
김보성 <62016657+bocastle@users.noreply.github.com>
2025-03-16 01:18:23 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.js
@@ -0,0 +1,11 @@
+function solution(str_list, ex) {
+    var answer = '';
+    
+    str_list.map((item)=>{
+        
+        if(!item.includes(ex)){
+            answer += item
+        }
+    })
+    return answer;
+}
