60b02a39c5f60d3b3e84292f4b7a3fda1f60d8e5
김보성 <62016657+bocastle@users.noreply.github.com>
2025-03-18 00:44:19 +0900

    [level 0] Title: 7의 개수, Time: 2.30 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120912. 7의 개수/7의 개수.js
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.js
@@ -1,12 +1,8 @@
 function solution(array) {
     var answer = 0;
-    let _array = array.join(',','').replaceAll(',','')
- 
-    for(let i = 0; i<_array.length; i++){
-        if(_array[i] === '7'){
-            answer++
-        }
-    }
+    let _array = array.join('').split('7').length-1
+    console.log(_array)
+    
 
-    return answer;
+    return _array;
 }
