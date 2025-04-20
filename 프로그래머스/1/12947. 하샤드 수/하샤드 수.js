ce65c045ab15c2706e700a571e2c2fd3247a4daf
김보성 <62016657+bocastle@users.noreply.github.com>
2025-04-20 20:59:29 +0900

    [level 1] Title: 하샤드 수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -2,11 +2,10 @@ function solution(x) {
   
     let cul = 0;
     let num = String(x).split("")
-    console.log(num)
     num.map((item) =>{
      cul += Number(item);
     })
-    console.log(cul)
+    // console.log(cul)
 
-    return x % cul === 0;;
+    return x % cul === 0;
 }
