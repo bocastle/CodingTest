9362d45c3cb076355a822236c444a24a323dee7a
김보성 <62016657+bocastle@users.noreply.github.com>
2025-04-23 23:25:03 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,9 +1,11 @@
 function solution(numbers) {
     let answer = 0;
-    let i = 0
+    
     for(let i = 0; i<=9; i++){
         if(!numbers.includes(i)){
+            
             answer = i + answer
+            
         }
     }
     
