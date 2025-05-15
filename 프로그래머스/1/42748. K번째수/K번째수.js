afbc3616d218298eef8d6f9cc7604d4bf8543a39
김보성 <62016657+bocastle@users.noreply.github.com>
2025-05-16 00:48:50 +0900

    [level 1] Title: K번째수, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,12 @@
+function solution(array, commands) {
+    var answer = [];
+    
+    for( let i = 0; i< commands.length; i++){
+        let temp = array.slice(commands[i][0] === 0 ? 0 : commands[i][0]-1 , commands[i][1]).sort((a, b) => a - b);
+        let arrayIndex = commands[i][2] - 1
+        answer.push(temp[arrayIndex])
+    }
+    
+    
+    return answer;
+}
