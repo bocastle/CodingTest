5855a49d5a922bed4066f9ea4bb1d1587736bec5
김보성 <62016657+bocastle@users.noreply.github.com>
2025-05-31 21:22:05 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,13 @@
+function solution(s) {
+  const numberWords = [
+    "zero", "one", "two", "three", "four",
+    "five", "six", "seven", "eight", "nine"
+  ];
+
+  numberWords.forEach((word, index) => {
+    const regex = new RegExp(word, "g");
+    s = s.replace(regex, index);
+  });
+
+  return Number(s);
+}
