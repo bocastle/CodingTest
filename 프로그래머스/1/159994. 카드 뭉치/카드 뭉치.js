0dd000d024cff089271c0864755cac529c319bd4
김보성 <62016657+bocastle@users.noreply.github.com>
2025-06-10 19:31:38 +0900

    [level 1] Title: 카드 뭉치, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,7 +1,6 @@
 function solution(cards1, cards2, goal) {
     let i = 0; 
     let j = 0; 
-    
     for (let word of goal) {
         if (i < cards1.length && cards1[i] === word) {
             i++; 
@@ -11,6 +10,5 @@ function solution(cards1, cards2, goal) {
             return "No"; 
         }
     }
-    
     return "Yes"; 
 }
